c0bb1e02ef11606d232ada50a1300d37f48bbdef
Philippe Marguinaud <pmarguinaud@hotmail.com>
2019-09-17 17:06:19 +0200

    Prune

---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd cpu
+make ARCH=CPU -f ../makefile
+
+for i in $(seq 10)
+do
+./wrap_acpcmt.x   --case ../t0031 --print --diff --diff-block-list 1 > out.txt 2>&1
+if [ $i -eq 1 ]
+then
+  \mv -f out.txt out.txt.ref
+else
+diff out.txt out.txt.ref
+fi
+done
+
+cd ..
+
+
+cd gpu
+make ARCH=GPU -f ../makefile
+
+for i in $(seq 10)
+do
+./wrap_acpcmt.x   --case ../t0031 --print --diff --diff-block-list 1 > out.txt 2>&1
+set +e
+diff ../cpu/out.txt out.txt
+c=$?
+set -e
+if [ $c -ne 0 ]
+then
+break
+fi
+done
+
+cd ..
+
+
+vim -d */out.txt
+
