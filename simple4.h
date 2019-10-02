e044bdc2373a50d75274516cbc9ee82838031af1
Hck Course <florian.suzat@meteo.fr>
2019-10-02 09:40:58 +0200

    nvvlink error bis

---
+++ simple4.h
@@ -0,0 +1,16 @@
+#ifdef ACC
+!$acc routine(ABOR1) seq
+!$acc routine(ACMICRO) seq
+!$acc routine(ACADVEC) seq
+!$acc routine(ACMTDDD) seq
+!$acc routine(ACMTENTR) seq
+!$acc routine(ACMTUD) seq
+!$acc routine(ACNEBSM) seq
+!$acc routine(ACNEBXRS) seq
+!$acc routine(ACPCMT) seq
+!$acc routine(ACPLUIZ) seq
+!$acc routine(ADVPRCS) seq
+!$acc routine(FCGENERALIZED_GAMMA) seq
+!$acc routine(FPCINCAPE) seq
+#endif
+
